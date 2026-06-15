e1e3a1fd0f2ea9adb2abf51c69701d26f5e75d2b
jana-tech0 <janu.incloud@gmail.com>
2026-06-15 22:13:14 +0530

    jenkins

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,11 +1,11 @@
 module "catalogue_instance" {
-  source = "git::https://github.com/jana-tech0/terraform-modules.git"
+  source = "source = "git::https://github.com/jana-tech0/terraform-modules.git//modules/ec2""
   ami = "ami-0f38e4c6b228846b1"
 
   instance_type = "t3.medium"
   vpc_security_group_ids = [data.aws_ssm_parameter.catalogue_sg_id.value]
   # it should be in Roboshop DB subnet
-  subnet_id = split(data.aws_ssm_parameter.private_subnet_ids,0)
+  subnet_id = split(data.aws_ssm_parameter.private_subnet_ids.value, ",")[0]
   user_data = file("catalogue.sh")
   tags = merge(
     {     
