bcb78dcad3141a94f916db65c02578a561207cec
jana-tech0 <janu.incloud@gmail.com>
2026-06-16 11:20:46 +0530

    jenkins

--- terraform/data.tf
+++ terraform/data.tf
@@ -35,23 +35,3 @@ data "aws_ami" "devops_ami" {
   }
 } 
 
-data "aws_ami" "devops_ami" {
-  most_recent      = true
-  name_regex       = "Centos-8-DevOps-Practice"
-  owners           = ["973714476881"]
-
-  filter {
-    name   = "name"
-    values = ["Centos-8-DevOps-Practice"]
-  }
-
-  filter {
-    name   = "root-device-type"
-    values = ["ebs"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-}
