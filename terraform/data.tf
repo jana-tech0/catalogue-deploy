770a46943139da5a26925a4d87fc92899cb57abd
jana-tech0 <janu.incloud@gmail.com>
2026-06-16 11:29:48 +0530

    jenkins

--- terraform/data.tf
+++ terraform/data.tf
@@ -14,24 +14,5 @@ data "aws_ssm_parameter" "app_alb_listener_arn" {
   name = "/${var.project_name}/${var.env}/app_alb_listener_arn"
 }
 
-data "aws_ami" "devops_ami" {
-  most_recent      = true
-  name_regex       = "Centos-8-DevOps-Practice"
-  owners           = ["973714476881"]
 
-  filter {
-    name   = "name"
-    values = ["Centos-8-DevOps-Practice"]
-  }
-
-  filter {
-    name   = "root-device-type"
-    values = ["ebs"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-} 
 
