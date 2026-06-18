06d3fb9615abd3cb8f23bdd766ace70405092b4b
jana-tech0 <janu.incloud@gmail.com>
2026-06-18 07:05:41 +0530

    jenkins

--- terraform/catalogue.sh
+++ terraform/catalogue.sh
@@ -1,3 +1,4 @@
+APP_VERSION=$1
 yum install python3.11-devel python3.11-pip -y
 pip3.11 install ansible botocore boto3
 cd /tmp
--- terraform/main.tf
+++ terraform/main.tf
@@ -4,7 +4,7 @@ module "catalogue_instance" {
   ami = data.aws_ami.password_enabled.id
   instance_type = "t3.micro"
   vpc_security_group_ids = [data.aws_ssm_parameter.catalogue_sg_id.value]
-  # it should be in Roboshop DB subnet
+  
   subnet_id = element(split(",",data.aws_ssm_parameter.private_subnet_ids.value), 0)
   //iam_instance_profile = "catalogue-profile-${var.env}"
   //user_data = file("catalogue.sh")
@@ -14,4 +14,33 @@ module "catalogue_instance" {
     },
     var.common_tags
   )
+}
+
+resource "null_resource" "cluster" {
+  
+  triggers = {
+    instance_id = module.catalogue_instance.id
+  }
+
+ 
+  connection {
+    type     = "ssh"
+    user     = "centos"
+    password = "DevOps321"
+    host     = module.catalogue_instance.private_ip
+  }
+
+  
+  provisioner "file" {
+    source      = "catalogue.sh"
+    destination = "/tmp/catalogue.sh"
+  }
+
+  provisioner "remote-exec" {
+   
+    inline = [
+      "chmod +x /tmp/catalogue.sh",
+      "sudo sh /tmp/catalogue.sh ${var.app_version} ${var.env}"
+    ]
+  }
 }
